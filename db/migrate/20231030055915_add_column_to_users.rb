55d531e08ecdcb31974fedb157c2d9e5e5fb68cd
ShirouzuHaruka <2111140077m@ed.fuk.kindai.ac.jp>
2023-11-01 01:51:46 +0000

    push to render.com

--- db/migrate/20231030055915_add_column_to_users.rb
+++ db/migrate/20231030055915_add_column_to_users.rb
@@ -1,4 +1,4 @@
-class AddColumnToUser < ActiveRecord::Migration[7.0]
+class AddColumnToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :age, :integer
   end
--- db/migrate/20231031015920_add_column_to_tweets.rb
+++ db/migrate/20231031015920_add_column_to_tweets.rb
@@ -1,4 +1,4 @@
-class AddColumnToTweet < ActiveRecord::Migration[7.0]
+class AddColumnToTweets < ActiveRecord::Migration[7.0]
   def change
     add_column :tweets, :user_id, :integer
   end
